89ffc8d08dd312753d3df08b4fc92ec0b9b45326
Emerson Mistico <102876556+Emerson-Mistico@users.noreply.github.com>
2025-09-28 16:57:51 -0300

    Change gun and UI update
    press C to change gun (with list) and UI update

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -1,31 +1,53 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections.Generic;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-    public GunBase gunBase;
+    [Header("Gun Basic SETUP")]
+    public List<GunBase> gunList;
     public Transform gunPosition;
+    public TextMeshProUGUI uiTextCurrentGun;
+    public int initialGun = 0;
 
     private GunBase _currentGun;
-
+    [SerializeField] private int _gunIndex = 0;
 
     protected override void Init()
     {
         base.Init();
 
-        CreateGun();
+        CreateGun(initialGun);
 
         inputs.Gameplay.Shoot.performed += cts => StartShoot();
         inputs.Gameplay.Shoot.canceled += cts => CancelShoot();
 
     }
 
-    private void CreateGun()
+    private void Update()
     {
-        _currentGun = Instantiate(gunBase, gunPosition);
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            _gunIndex++;
+
+            if (_gunIndex >= gunList.Count)
+            {
+                _gunIndex = 0;
+            }
+
+            CreateGun(_gunIndex);
+        }
+    }
+
+
+    private void CreateGun(int gunToCreate)
+    {
+        _currentGun = Instantiate(gunList[gunToCreate], gunPosition);
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        uiTextCurrentGun.text = gunList[gunToCreate].tag.ToString();
+        //Debug.Log("Current GUN: " + gunList[gunToCreate].tag);
     }
    
     private void StartShoot()
@@ -38,6 +60,5 @@ public class PlayerAbilityShoot : PlayerAbilityBase
         //Debug.Log("-> Cancel Shoot");
         _currentGun.StopShoot();
     }
-
 
 }
